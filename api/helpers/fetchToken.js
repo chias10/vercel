aac91c2cc953e563f4f928091240692f5c843092
chias10 <34822726+chias10@users.noreply.github.com>
2025-04-15 14:53:50 -0600

    Update fetchToken.js

--- api/helpers/fetchToken.js
+++ api/helpers/fetchToken.js
@@ -1,21 +1,20 @@
-// /api/helpers/fetchToken.js
 export async function obtenerToken() {
   try {
-    // Hacer una solicitud para obtener el token
+    // Realiza la solicitud para obtener el token
     const loginResponse = await fetch('http://ec2-34-209-178-62.us-west-2.compute.amazonaws.com:4000/api/session', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        email: "aziel.cuevasf@gmail.com", // Ajusta esto con tus credenciales
-        password: "IZMXs6c1Usb8fQ886J"   // Ajusta esto con tus credenciales
+        email: "aziel.cuevasf@gmail.com", // Cambia esto por tu email
+        password: "IZMXs6c1Usb8fQ886J"   // Cambia esto por tu contraseña
       })
     });
 
     const loginData = await loginResponse.json();
 
-    // Verificar si se obtuvo el token
+    // Verifica que se haya obtenido el token
     if (!loginResponse.ok || !loginData.token) {
       throw new Error('No se pudo obtener el token');
     }
