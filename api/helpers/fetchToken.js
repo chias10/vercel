0fb40e4f406de69f30e9f2f66b68847b4cfe3d59
chias10 <34822726+chias10@users.noreply.github.com>
2025-04-15 15:00:09 -0600

    Update fetchToken.js

--- api/helpers/fetchToken.js
+++ api/helpers/fetchToken.js
@@ -1,5 +1,14 @@
 export async function obtenerToken() {
   try {
+    // Usar variables de entorno para el email y la contraseña
+    const email = process.env.EMAIL;
+    const password = process.env.PASSWORD;
+    
+    // Verificar que las variables de entorno estén definidas
+    if (!email || !password) {
+      throw new Error('Las credenciales de acceso no están configuradas correctamente.');
+    }
+
     // Realiza la solicitud para obtener el token
     const loginResponse = await fetch('http://ec2-34-209-178-62.us-west-2.compute.amazonaws.com:4000/api/session', {
       method: 'POST',
@@ -7,20 +16,28 @@ export async function obtenerToken() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        email: "aziel.cuevasf@gmail.com", // Cambia esto por tu email
-        password: "IZMXs6c1Usb8fQ886J"   // Cambia esto por tu contraseña
+        email,  // Utiliza las variables de entorno
+        password
       })
     });
 
+    // Verifica si la respuesta fue exitosa
+    if (!loginResponse.ok) {
+      const errorDetails = await loginResponse.text(); // O puedes usar .json() si la respuesta es JSON
+      throw new Error(`Error al obtener el token: ${errorDetails}`);
+    }
+
     const loginData = await loginResponse.json();
 
-    // Verifica que se haya obtenido el token
-    if (!loginResponse.ok || !loginData.token) {
+    // Verifica que el token esté presente en la respuesta
+    if (!loginData || !loginData.token) {
       throw new Error('No se pudo obtener el token');
     }
 
+    // Devuelve el token
     return loginData.token;
   } catch (error) {
+    // Proporciona un mensaje de error detallado
     throw new Error('Error al obtener el token: ' + error.message);
   }
 }
