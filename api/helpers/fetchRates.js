cf325e9d3397b2e6f4b6e1280f62243d5d9e5bf4
chias10 <34822726+chias10@users.noreply.github.com>
2025-04-15 14:54:17 -0600

    Update fetchRates.js

--- api/helpers/fetchRates.js
+++ api/helpers/fetchRates.js
@@ -1,35 +1,42 @@
-// /api/helpers/fetchRates.js
 export async function obtenerTarifas(token, datos) {
-  const response = await fetch('http://ec2-34-209-178-62.us-west-2.compute.amazonaws.com:4000/api/rates', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    },
-    body: JSON.stringify({
-      address_from: {
-        country_code: "MX",
-        zip_code: datos.origen
+  try {
+    // Realiza la solicitud de tarifas utilizando el token
+    const response = await fetch('http://ec2-34-209-178-62.us-west-2.compute.amazonaws.com:4000/api/quote', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
       },
-      address_to: {
-        country_code: "MX",
-        zip_code: datos.destino
-      },
-      parcel: {
-        currency: "MXN",
-        distance_unit: "CM",
-        height: parseFloat(datos.alto),
-        length: parseFloat(datos.largo),
-        mass_unit: "KG",
-        weight: parseFloat(datos.peso),
-        width: parseFloat(datos.ancho)
-      }
-    })
-  });
+      body: JSON.stringify({
+        type: "quote",
+        data: {
+          shipment: {
+            carrier: "estafeta,paquetexpress,fedex,redpack,dhl",
+            type: "standard"
+          },
+          parcel: {
+            weight: parseFloat(datos.peso),
+            height: parseFloat(datos.alto),
+            width: parseFloat(datos.ancho),
+            length: parseFloat(datos.largo)
+          },
+          origin: {
+            zip: datos.origen
+          },
+          destination: {
+            zip: datos.destino
+          }
+        }
+      });
 
-  if (!response.ok) {
-    throw new Error('Error al obtener las tarifas');
+    const tarifaData = await response.json();
+
+    if (!response.ok) {
+      throw new Error('Error al obtener las tarifas');
+    }
+
+    return tarifaData;
+  } catch (error) {
+    throw new Error('Error al obtener las tarifas: ' + error.message);
   }
-
-  return await response.json();
 }
