faf571bdcf2b12cc0ad0eff1dfeb2fb3bc23a1a0
chias10 <34822726+chias10@users.noreply.github.com>
2025-04-15 16:23:53 -0600

    Update cotizar.js

--- api/cotizar.js
+++ api/cotizar.js
@@ -9,29 +9,29 @@ export default async function handler(req, res) {
      console.log('Token:', token);
     console.log('Datos:', datos);
 
-  const response = await fetch('http://ec2-54-188-18-143.us-west-2.compute.amazonaws.com:4000/api/rates', {
+  const response = await fetch('http://ec2-34-209-178-62.us-west-2.compute.amazonaws.com:4000/api/rates', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
+      'Authorization': "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJtYW51YWJsZV9hcGkiLCJleHAiOjE3NDQ4Mzk5MzIsImlhdCI6MTc0NDc1MzUzMiwiaXNzIjoibWFudWFibGVfYXBpIiwianRpIjoiMDMwNjI5MTQtZGZiMi00MTBmLWJhNGUtZDJjNWU3OGEyODc2IiwibmJmIjoxNzQ0NzUzNTMxLCJzdWIiOiI3IiwidHlwIjoiYWNjZXNzIn0.1Pqn_9XMkLKRNMjTZ2DthNingEEU0ARiz1N9g45JfTz0_Zb3denPJxFvJjes2eRHNOAZYbmt_atUaahibK_Hiw"
     },
     body: JSON.stringify({
       address_from: {
         country_code: "MX",
-        zip_code: datos.origen // Por ejemplo: "54040"
+        zip_code: "37700" // Por ejemplo: "54040"
       },
       address_to: {
         country_code: "MX",
-        zip_code: datos.destino // Por ejemplo: "54040"
+        zip_code: "01010" // Por ejemplo: "54040"
       },
       parcel: {
         currency: "MXN",
         distance_unit: "CM",
-        height: parseFloat(datos.alto),
-        length: parseFloat(datos.largo),
+        height: 10,
+        length: 10,
         mass_unit: "KG",
-        weight: parseFloat(datos.peso),
-        width: parseFloat(datos.ancho)
+        weight: 5,
+        width: 10
       }
     })
   });
